ef68c2e02b0f9180da150cca7164e6e436bc67c1
LShubinaDev <lisaShubina2003@yandex.ru>
2021-03-18 02:29:36 +0300

    Digital Design Task

--- src/test/java/digital/design/repeatin/strings/handler/unit/validation/DefaultInputValidatorTest.java
+++ src/test/java/digital/design/repeatin/strings/handler/unit/validation/DefaultInputValidatorTest.java
@@ -9,74 +9,84 @@ import static org.junit.Assert.*;
  * @version 1.0
  */
 public class DefaultInputValidatorTest {
-    private final DefaultInputValidator inputValidator = new DefaultInputValidator("[a-zA-Z0-9\\[\\]]*");
+    private final DefaultInputValidator validator = new DefaultInputValidator("[a-zA-Z0-9\\[\\]]*");
 
     /* INVALID INPUTS */
 
     @Test
     public void npe() {
-        assertThrows(NullPointerException.class, () -> inputValidator.validate(null));
+        assertThrows(NullPointerException.class, () -> validator.validate(null));
     }
 
     @Test
     public void containsIllegalCyrillicCharacters() {
-        assertThrows(IllegalArgumentException.class, () -> inputValidator.validate("aбвгcde[p]"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("aбвгcde[p]"));
     }
 
     @Test
     public void containsIllegalSymbols() {
-        assertThrows(IllegalArgumentException.class, () -> inputValidator.validate("_$123[]"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("_$123[]"));
     }
 
     @Test
     public void startsWithOpeningBracket() {
-        assertThrows(IllegalArgumentException.class, () -> inputValidator.validate("[10[abc]"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("[10[abc]"));
     }
 
     @Test
     public void onlyOneClosingBracket() {
-        assertThrows(IllegalArgumentException.class, () -> inputValidator.validate("abs10]"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("abs10]"));
     }
 
     @Test
     public void onlyOpeningBracket() {
-        assertThrows(IllegalArgumentException.class, () -> inputValidator.validate("aa1["));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("aa1["));
     }
 
     @Test
     public void digitsBetweenBrackets() {
-        assertThrows(IllegalArgumentException.class, () -> inputValidator.validate("a10[a1b10[a]]"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("a10[a1b10[a]]"));
     }
 
     @Test
     public void wrongBracketsOrder() {
-        assertThrows(IllegalArgumentException.class, () -> inputValidator.validate("]abc["));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("]abc["));
     }
 
     @Test
     public void openingAndClosingBracketsAmountDoesntMatch() {
-        assertThrows(IllegalArgumentException.class, () -> inputValidator.validate("[[]abс]extraClosingBracket]"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("[[]abс]extraClosingBracket]"));
     }
 
     @Test
     public void noDigitsBeforeOpeningBracket() {
-        assertThrows(IllegalArgumentException.class, () -> inputValidator.validate("abc[abc]"));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("abc[abc]"));
     }
 
     @Test
     public void emptyStringInput() {
-        assertThrows(IllegalArgumentException.class, () -> inputValidator.validate(""));
+        assertThrows(IllegalArgumentException.class, () -> validator.validate(""));
+    }
+
+    @Test
+    public void singleSlash() {
+        assertThrows(IllegalArgumentException.class, () -> validator.validate("/"));
     }
 
     /* VALID INPUTS */
 
     @Test
     public void validInputNoBrackets() {
-        inputValidator.validate("abcdefghijklmnopqrstuvwxyABCDEFGHIJKLOMNOPQRSTUVWXUZ");
+        validator.validate("abcdefghijklmnopqrstuvwxyABCDEFGHIJKLOMNOPQRSTUVWXUZ");
     }
 
     @Test
     public void easyValidInput() {
-        inputValidator.validate("ab1[c]");
+        validator.validate("ab1[c]");
+    }
+
+    @Test
+    public void hard() {
+        validator.validate("a1[b2[c]d4[d5[ef6[g]]]]");
     }
 }
